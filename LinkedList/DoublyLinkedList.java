b9612f2f07409b4b3b85c176cab028d81039b576
Eliza Sarah <135806618+elizaasaarah@users.noreply.github.com>
2023-07-04 09:34:04 +0530

    feat: doubly linked list

---
+++ LinkedList/DoublyLinkedList.java
@@ -0,0 +1,135 @@
+public class DoublyLinkedList {
+    DoublyLinkedList() {
+        head = null;
+    }
+    
+    Node head;
+    class Node {
+        int val;
+        Node next, prev;
+
+        Node(int val) {
+            this.val = val;
+        }
+
+        Node(int val, Node next, Node prev) {
+            this.val = val;
+            this.next = next;
+            this.prev = prev;
+        }
+    }
+    
+    private int getLength() {
+        Node curr = head;
+        int length = 0;
+
+        while(curr != null) {
+            length++;
+            curr = curr.next;
+        }
+
+        return length;
+    }
+
+    public void addAtHead(int val) {
+        Node newNode = new Node(val, head, null);
+        
+        if(head != null) {
+            head.prev = newNode;
+        }
+        head = newNode;
+    }
+
+    public void addAtTail(int val) {
+        if(head == null) {
+            addAtHead(val);
+            return;
+        }
+
+        Node curr = head;
+        while(curr.next != null) {
+            curr = curr.next;
+        }
+
+        curr.next = new Node(val, null, curr);
+    }
+
+    public void addAtIndex(int index, int val) {
+        int length = getLength();
+        if(index < 0 || index > length) {
+            return;
+        }
+
+        if(index == 0) {
+            addAtHead(val);
+            return;
+        }
+
+        if(index == length) {
+            addAtTail(val);
+            return;
+        }
+
+        int currIndex = 0;
+        Node curr = head;
+
+        while(currIndex != index) {
+            curr = curr.next;
+            currIndex++;
+        }
+
+        Node prev = curr.prev;
+        Node newNode = new Node(val, curr, prev);
+        if(prev != null)
+            prev.next = newNode;
+        if(curr != null)
+            curr.prev = newNode;
+    }
+
+    public int get(int index) {
+        int length = getLength();
+        if(index < 0 || index >= length) {
+            return -1;
+        }
+
+        int currIndex = 0;
+        Node curr = head;
+
+        while(currIndex != index) {
+            currIndex++;
+            curr = curr.next;
+        }
+
+        return curr.val;
+    }
+
+    public void deleteAtIndex(int index) {
+        int length = getLength();
+        if(head == null || index < 0 || index >= length) {
+            return;
+        }
+
+        if(index == 0) {
+            head = head.next;
+            return;
+        }
+
+        int currIndex = 0;
+        Node curr = head;
+
+        while(currIndex != index) {
+            currIndex++;
+            curr = curr.next;
+        }
+
+        Node prev = curr.prev;
+        Node next = curr.next;
+
+        if(prev != null) {
+            prev.next = next;
+        }
+        if(next != null) {
+            next.prev = prev;
+        }
+    }
+}
